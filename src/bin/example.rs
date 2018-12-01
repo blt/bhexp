3c55365affd504889e3de7c9f58355ea142b34b2
Brian L. Troutwine <brian@troutwine.us>
2018-11-30 23:16:11 -0800

    Initial commit.
    Demonstrate the use of bughunt-core. Build like `cargo-bughunt
    build`. This assumes you've previously installed `cargo-bughunt` from
    the blt/bughunt repository. Run like
    `./target/release/example`. Nothing will happen, save that a static
    table inside the blt/bughunt-core library will get updated with guard
    counts from example.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

---
+++ src/bin/example.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate bughunt_core;
+
+fn main() {
+    test!(|data: &[u8]| {
+        if data.len() != 6 {
+            return;
+        }
+        if data[0] != b'q' {
+            return;
+        }
+        if data[1] != b'w' {
+            return;
+        }
+        if data[2] != b'e' {
+            return;
+        }
+        if data[3] != b'r' {
+            return;
+        }
+        if data[4] != b't' {
+            return;
+        }
+        if data[5] != b'y' {
+            return;
+        }
+        panic!("BOOM")
+    })
+}
